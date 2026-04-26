82a69e45cf537c3f4d3410d1dbb41c94ad8fd927
lukmanproject <lukmanoman17@gmail.com>
2026-04-26 17:50:40 +0800

    Menambahkan routing alpha, alphanum, dan route group api

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,23 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// Route default (halaman beranda)
+$routes->get('/', 'Beranda::index');
+// Route halaman tentang
+$routes->get('tentang', 'Beranda::tentang');
+// Route dengan parameter numerik
+$routes->get('pengguna/(:num)', ' Beranda::pengguna/$1');
+// Route halaman waktu
+$routes->get('waktu', 'Home::waktu');
+
+$routes->get('akademik', 'Akademik::index');
+$routes->get('akademik/matkul', 'Akademik::matkul');
+$routes->get('akademik/nilai/(:num)', 'Akademik::nilai/$1');
+
+$routes->get('test/alpha/(:alpha)', 'Akademik::index/$1');
+$routes->get('test/alphanum/(:alphanum)', 'Akademik::index/$1');
+
+$routes->group('api', function ($routes) {
+    $routes->get('status', 'Akademik::index');
+    $routes->get('versi', 'Akademik::matkul');
+});
--- app/Controllers/Akademik.php
+++ app/Controllers/Akademik.php
@@ -7,8 +7,29 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 class Akademik extends BaseController
 {
-    public function index()
+    public function index(): string
     {
-        //
+        return "<h1>Sistem Informasi Akademik</h1>
+                <p>Nama: Lukman Hakim</p>";
+    }
+
+    public function matkul(): string
+    {
+        return "
+        <h2>Mata Kuliah</h2>
+        <ul>
+            <li>Pemrograman Web 2</li>
+            <li>Basis Data</li>
+            <li>OOP</li>
+            <li>Struktur Data</li>
+            <li>Jaringan Komputer</li>
+        </ul>
+        ";
+    }
+
+    public function nilai($nim): string
+    {
+        return "<h2>Nilai Mahasiswa</h2>
+                <p>NIM: $nim</p>";
     }
 }
