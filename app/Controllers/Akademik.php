8000ae28c11baa8df937826d16dac35fa36eea0c
lukmanproject <lukmanoman17@gmail.com>
2026-04-26 17:40:00 +0800

    Membuat controller akademik

---
+++ app/Controllers/Akademik.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Akademik extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
