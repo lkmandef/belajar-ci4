b6b9f2789ae69fcdb555c0d27b92da86dfa96b61
lukmanproject <lukmanoman17@gmail.com>
2026-04-27 10:10:14 +0800

    Pertemuan 2: Templating dan layouting view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,10 +10,10 @@ $routes->get('/', 'Beranda::index');
 // Route halaman tentang
 $routes->get('tentang', 'Beranda::tentang');
 // Route dengan parameter numerik
-$routes->get('pengguna/(:num)', ' Beranda::pengguna/$1');
+$routes->get('pengguna/(:num)', 'Beranda::pengguna/$1');
 // Route halaman waktu
 $routes->get('waktu', 'Home::waktu');
-
+// Route Latihan
 $routes->get('akademik', 'Akademik::index');
 $routes->get('akademik/matkul', 'Akademik::matkul');
 $routes->get('akademik/nilai/(:num)', 'Akademik::nilai/$1');
@@ -21,7 +21,5 @@ $routes->get('akademik/nilai/(:num)', 'Akademik::nilai/$1');
 $routes->get('test/alpha/(:alpha)', 'Akademik::index/$1');
 $routes->get('test/alphanum/(:alphanum)', 'Akademik::index/$1');
 
-$routes->group('api', function ($routes) {
-    $routes->get('status', 'Akademik::index');
-    $routes->get('versi', 'Akademik::matkul');
-});
+// Route controller Demo
+$routes->get('demo', 'Demo::index');
